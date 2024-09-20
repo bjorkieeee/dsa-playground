b1e89166eb990135e923f1f5e03eafdb27a29a96
bjorkieeee <bjorkarang981@gmail.com>
2024-09-20 00:42:18 -0300

    Finished binary search in C :)

--- binary_search.c
+++ binary_search.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 
-
 int binarySearch(int targetNum, int array[], int size){
     // must return the position of the target number in the array
     // Currently failing because I went on an infinite loop
@@ -10,7 +9,7 @@ int binarySearch(int targetNum, int array[], int size){
     int middle;
                                                            
     while (low <= high){
-        middle = high / 2;
+        middle = (low + high) / 2;
         if (array[middle] == targetNum){
             return middle;
         } else if (array[middle] > targetNum){
@@ -19,14 +18,14 @@ int binarySearch(int targetNum, int array[], int size){
             low = middle + 1;
         }
     }
-    return middle;
+    return -1;
 }
 
 int main(){
     int myNumbers[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int size = sizeof(myNumbers) / sizeof(myNumbers[0]); // sizeof measures the size of the items in bytes
-    int answer = binarySearch(3, myNumbers, size);
+    int answer = binarySearch(-2, myNumbers, size);
     printf("%d\n", answer);
 
-    return 0;
+    return answer;
 }
